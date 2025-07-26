abd1e6d508b5006f1e65559c7e72cf764382130c
Mathilde4477 <visites@immersive-normandy.com>
2025-07-26 20:51:19 +0200

    Update interface_formulaire.py

--- interface_formulaire.py
+++ interface_formulaire.py
@@ -28,16 +28,27 @@ langue = st.selectbox("Langue / Language", ["Français", "Anglais"] if langue_ui
 niveau_scolaire = st.text_input("Niveau scolaire")
 nombre_personnes = st.number_input("Nombre de personnes", min_value=1, step=1)
 capacite_max = st.number_input("Capacité max", min_value=1, step=1)
-programme = st.selectbox("Programme", [
-    "Plages du Débarquement (secteur US)",
-    "Plages du Débarquement (secteur GB)",
-    "Plages du Débarquement (secteur Canadien)",
-    "Plages du Débarquement (US/GB)",
-    "Mont Saint Michel",
-    "Vieux Bayeux et Cathédrale",
-    "Médiéval",
-    "Autre"
-] if langue_ui == "Français" else [
+programme = st.selectbox("Programme",
+    [
+        "Plages du Débarquement (secteur US)",
+        "Plages du Débarquement (secteur GB)",
+        "Plages du Débarquement (secteur Canadien)",
+        "Plages du Débarquement (US/GB)",
+        "Mont Saint Michel",
+        "Vieux Bayeux et Cathédrale",
+        "Médiéval",
+        "Autre"
+    ] if langue_ui == "Français" else [
+        "D-Day beaches (US sector)",
+        "D-Day beaches (British sector)",
+        "D-Day beaches (Canadian sector)",
+        "D-Day beaches (US/GB)",
+        "Mont Saint Michel",
+        "Old Bayeux and Cathedral",
+        "Medieval",
+        "Other"
+    ]
+)
     "D-Day beaches (US sector)",
     "D-Day beaches (British sector)",
     "D-Day beaches (Canadian sector)",
